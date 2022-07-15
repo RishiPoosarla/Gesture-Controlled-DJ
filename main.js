16990e2fede5b3dad9f3b2ae23d46486b9ae9b14
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-07-14 19:45:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelloaded);
+    posenet.on("pose", gotposes);
 }
 function preload(){
     peterPanSong = loadSound("music.mp3");
@@ -12,4 +14,18 @@ function preload(){
 }
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+function modelloaded(){
+    console.log("Posenet is initialized");
+}
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX: " + leftWristX + "leftWristY: "  + leftWristY);
+        console.log("lrightWristX: " + rightWristX + "rightWristY: "  + rightWristY);
+    }
 }
